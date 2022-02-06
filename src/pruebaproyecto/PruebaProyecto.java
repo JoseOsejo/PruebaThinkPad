beb5fc8c54d5bbba093c5154167e274581edbe4c
Jose Carlos Osejo <jcoq2003@gmail.com>
2022-02-06 17:22:47 -0600

    subiendo prueba proyecto

---
+++ src/pruebaproyecto/PruebaProyecto.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebaproyecto;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jcoq2
+ */
+public class PruebaProyecto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        System.out.println("Hola Mundo");
+        System.out.println("Por fin pude descargarme Netbeans 8.2!");
+        System.out.println("El netbeans de Apache es la peor mierda que puede haber");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese su opinion sobre Apache Netbeans ");
+        String opinion = sc.next();
+        // TODO code application logic here
+    }
+    
+}
